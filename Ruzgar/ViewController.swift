34a3c8e9f2b114b847ce2938e4fe35869f1b2abc
aybek can kaya <aybekcankaya@icloud.com>
2016-01-19 10:08:40 +0200

    Initial Commit

---
+++ Ruzgar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ruzgar
+//
+//  Created by aybek can kaya on 19/01/16.
+//  Copyright © 2016 aybek can kaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
